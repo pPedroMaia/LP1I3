85b9cc965898b5d13cc42b3ad4dea1603ec2f1a6
pPedroMaia <108035985+pPedroMaia@users.noreply.github.com>
2023-05-29 16:03:05 -0300

    Add files via upload

---
+++ LP1I3 - Trabalho Pratico 04/MainData.java
@@ -0,0 +1,12 @@
+/*
+*Pedro henrique de Oliveira Maia dos Santos CB3013197
+*/
+public class MainData {
+	public static void main(String[] args) {
+		Data data1 = new Data();
+		data1.mostra1();
+		data1.mostra2();
+		data1.diasTranscorridos();
+		data1.apresentaDataAtual();
+	}
+}
